13da1539874da5a170a9edb3a00b9a41a47bb772
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-09-27 00:14:52 +0530

    Design Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
+import Home from './pages/Home/Home.jsx'
 
 const App = () => {
   return (
 
     <div className='app'>
+
       <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Cart' element={<Cart />} />
+        <Route path='/PlaceOrder' element={<PlaceOrder />} />
+      </Routes>
     </div>
 
   )
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      
+      <div className='header-contents'>
+
+        <h2>Order Your Fav Dish Here...</h2>
+        <p>"Freshly prepared, Fast delivered - Satisfy your cravings with just a few clicks!"</p>
+        <button>View Menu</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
