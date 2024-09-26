d8cf93a032cc0494a2da7d2f58555add79ef5562
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-09-27 01:00:54 +0530

    Add menu component & images

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,10 +2,55 @@ import frontImage from './frontImage.jpg'
 import search from './search.png'
 import bag from './bag.png'
 import logo from './logo.png'
+import menu1 from './menu1.jpg'
+import menu2 from './menu2.jpg'
+import menu3 from './menu3.jpg'
+import menu4 from './menu4.jpg'
+import menu5 from './menu5.jpg'
+import menu6 from './menu6.jpg'
+import menu7 from './menu7.jpg'
 
 export const assets = {
     frontImage,
     search,
     bag,
-    logo
+    logo,
+    menu1,
+    menu2,
+    menu3,
+    menu4,
+    menu5,
+    menu6,
+    menu7
 }
+
+export const menu_list = [
+    {
+        menu_name: "Cake",
+        menu_image: menu1
+    },
+    {
+        menu_name: "Desserts",
+        menu_image: menu2
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu3
+    },
+    {
+        menu_name: "Pastry",
+        menu_image: menu4
+    },
+    {
+        menu_name: "Pizza",
+        menu_image: menu5
+    },
+    {
+        menu_name: "Salad",
+        menu_image: menu6
+    },
+    {
+        menu_name: "Sea Food",
+        menu_image: menu7
+    }
+]
