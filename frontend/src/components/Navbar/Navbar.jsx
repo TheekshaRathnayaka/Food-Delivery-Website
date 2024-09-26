73611f1c88273e83bcdd184ea0b1bfddf3f02927
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-09-26 14:35:32 +0530

    Complete Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("home");
+
   return (
     <div className="navbar">
 
       <img src={assets.logo} alt="" className="logo" />
 
       <ul className="navbar-menu">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Menu</a></li>
-        <li><a href="#">Mobile-App</a></li>
-        <li><a href="#">Contact</a></li>
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
       </ul>
       
       <div className="navbar-right">
