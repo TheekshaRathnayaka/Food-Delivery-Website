08e8566805c53a45a8da9b964b9445f03d521946
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-01 02:26:27 +0530

    Add app download section

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className='footer-content'>
 
             <div className='footer-content-left'>
-                <img src={assets.logo} alt='' />
+                <img className='logo' src={assets.logo} alt='' />
                 <p>"Freshly prepared, Fast delivered - Satisfy your cravings with just a few clicks!"</p>
                 <div className='footer-social-icons'>
                     <img src={assets.facebook} alt='' />
@@ -18,15 +18,26 @@ const Footer = () => {
             </div>
 
             <div className='footer-content-center'>
-
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
             </div>
 
             <div className='footer-content-right'>
-
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>011-456-9321</li>
+                    <li>yummyhub@gmail.com</li>
+                </ul>
             </div>
-
         </div>
       
+      <hr />
+      <p className="footer-copyright">© YummyHub.com, 2024 Inc. All rights reserved.</p>
     </div>
   )
 }
