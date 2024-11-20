d781347a1f2a54958987fbc80baa0449e02ea85c
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-11-20 23:08:37 +0530

    Check user already exists

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -10,7 +10,15 @@ const loginUser = async (req,res) => {
 
 //register user
 const registerUser = async (req,res) => {
-
+    const {name,password,email} = req.body;
+    try {
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            return res.json({success:false, message:"User already exists"})
+        }
+    } catch (error) {
+        
+    }
 }
 
 export {loginUser, registerUser}
