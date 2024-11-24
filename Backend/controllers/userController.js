d131d09d4ec7808febd3cd9e7c0cffac9f3c6a16
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-11-24 23:19:40 +0530

    Login user error messages

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -12,9 +12,19 @@ const loginUser = async (req,res) => {
         if (!user) {
             return res.json({success:false, message:"User doesn't exist"})
         }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.json({success:false, message:"Invalid credentials"})
+        }
+
+        const token = createToken(user._id);
+        res.json({success:true, token});
         
     } catch (error) {
-        
+        console.log(error);
+        res.json({success:false, message:"Error"})
     }
 }
 
