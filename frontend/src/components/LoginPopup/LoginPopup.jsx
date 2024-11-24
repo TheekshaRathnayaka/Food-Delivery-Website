c7297a8fa55b90576bb8b884a355c4bb9d9b0b17
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-11-25 00:01:32 +0530

    Add values to login popup inputs

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -5,6 +5,11 @@ import { assets } from '../../assets/assets';
 const LoginPopup = ({setShowLogin}) => {
 
     const [currState, setCurrState] = useState('Login');
+    const [data, setData] = useState({
+      name:"",
+      email:"",
+      password:""
+    })
 
   return (
     <div className='login-popup'>
@@ -17,9 +22,9 @@ const LoginPopup = ({setShowLogin}) => {
         </div>
 
         <div className="login-popup-inputs">
-            {currState === 'Login'? <></>: <input type="text" placeholder='Your name' required />}
-            <input type="email" placeholder="Your email" required />
-            <input type="password" placeholder="Password" required />
+            {currState === 'Login'? <></>: <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Your name' required />}
+            <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder="Your email" required />
+            <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder="Password" required />
         </div>
 
         <button>{ currState === "Sign Up"? "Create account": "Login" }</button>
