105c5c5806fdbc79582c917f1d6b5ecba33010f4
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-12-10 20:55:36 +0530

    Take decoded userId

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -5,7 +5,14 @@ const authMiddleware = async (req, res, next) => {
     if (!token) {
         return res.json({success: false, message: "Not authorized login again"});
     }
-
+    try {
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+        req.body.userId = token_decode.id;
+        next();
+    } catch (error) {
+        
+    }
 }
 
 export default authMiddleware;
+//7.09
