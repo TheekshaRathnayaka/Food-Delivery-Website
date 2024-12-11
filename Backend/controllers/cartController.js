0d908fd445864d73175835a70f10c281e7187bb2
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-12-11 23:52:08 +0530

    Add cartData

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -4,7 +4,14 @@ import userModel from "../models/userModel.js"
 const addToCart = async (req,res) => {
     try {
         let userData = await userModel.findOne({_id:req.body.userId});
-       
+        let cartData = await userData.cartData;
+        if(!cartData[req.body.itemId])
+        {
+            cartData[req.body.itemId] = 1
+        }
+        else {
+            cartData[req.body.itemId] += 1;
+        }
     } catch (error) {
         
     }
