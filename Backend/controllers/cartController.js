6e301b81228252f4f7a1f832484464a7407b34a8
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-12-13 23:36:52 +0530

    Catch cart data

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -3,7 +3,7 @@ import userModel from "../models/userModel.js"
 //add items to user cart
 const addToCart = async (req,res) => {
     try {
-        let userData = await userModel.findOne({_id:req.body.userId});
+        let userData = await userModel.findById({req.body.userId});
         let cartData = await userData.cartData;
         if(!cartData[req.body.itemId])
         {
@@ -23,7 +23,11 @@ const addToCart = async (req,res) => {
 //remove items from user cart
 const removeFromCart = async (req,res) => {
     try {
-        let userData = await userModel.findById(req.body.userId)
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if (cartData[req.body.itemId]>0) {
+            cartData[req.body.itemId] -= 1;
+        }
     } catch (error) {
         
     }
