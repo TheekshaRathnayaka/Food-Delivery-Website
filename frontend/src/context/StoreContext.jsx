ceedf4777bb044143d13f1fa21b887046157dedf
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-12-17 23:35:59 +0530

    Add to database cart

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ const StoreContextProvider = (props) => {
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([])
 
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({ ...prev, [itemId]: 1}))
         }
