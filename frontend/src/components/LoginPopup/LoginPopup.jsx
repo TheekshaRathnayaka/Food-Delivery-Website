48d7600a790c846c6f1c7c6724e89a3f90606df5
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2025-01-27 21:26:13 +0530

    Import storecontext

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets';
-import { StoreContext } from '../../context/StoreContext';
-import axios from 'axios'
+import { StoreContext } from '../../context/StoreContext.jsx';
+import axios from 'axios';
 
 const LoginPopup = ({setShowLogin}) => {
 
